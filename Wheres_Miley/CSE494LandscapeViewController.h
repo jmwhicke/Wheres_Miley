0111d2eecc47fbc52fc7291727767e2ad613f2ed
Jorden Whicker <jmwhicke@asu.edu>
2013-10-27 16:41:02 -0700

    Test commit

--- Wheres_Miley/CSE494LandscapeViewController.h
+++ Wheres_Miley/CSE494LandscapeViewController.h
@@ -3,7 +3,7 @@
 //  Wheres_Miley
 //
 //  Created by Jorden Whicker on 10/23/13.
-//  Copyright (c) 2013 ASU. All rights reserved. dfsd
+//  Copyright (c) 2013 ASU. All rights reserved. dfsd test
 //
 
 #import <UIKit/UIKit.h>
